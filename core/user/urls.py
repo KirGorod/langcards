a866adef40c38fff0c1775cefb1e7d509e120cfd
KirGorod <kgorodisky@gmail.com>
2023-08-13 17:51:44 +0300

    user register endpoint

--- core/user/urls.py
+++ core/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, RegisterView
 
 app_name = 'user'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='user_login'),
+    path('register/', RegisterView.as_view(), name='user_register'),
 ]
--- core/user/views.py
+++ core/user/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
+from .serializers import UserRegisterSerializer
+
 
 class LoginView(APIView):
     def post(self, request):
@@ -13,6 +15,15 @@ class LoginView(APIView):
 
         if user:
             token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Wrong username or password'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'user_id': user.id}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
