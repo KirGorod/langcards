7fb4323312bfc4c2e4e02ea5d78600bc6549b8c6
KirGorod <kgorodisky@gmail.com>
2023-08-14 13:18:02 +0300

    update settings for prod

--- core/core/settings/base.py
+++ core/core/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8rr-&6l0++$#11e_ny4ivoeyexm6ebeyekv8ntga5l$bymur41'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ core/core/settings/dev.py
@@ -0,0 +1,6 @@
+from .base import *
+
+DEBUG = True
+
+# Other development-specific settings
+SECRET_KEY = "django-insecure-8rr-&6l0++$#11e_ny4ivoeyexm6ebeyekv8ntga5l$bymur41ss"
