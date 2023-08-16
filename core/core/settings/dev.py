98a5a4625d8cb78693974d34a76d097a3273ac3b
KirGorod <kgorodisky@gmail.com>
2023-08-16 18:30:08 +0300

    update actions conf

--- core/core/settings/dev.py
+++ core/core/settings/dev.py
@@ -1,7 +1,8 @@
+import os
 from .base import *
 
 DEBUG = True
 
 # Other development-specific settings
-SECRET_KEY = "django-insecure-8rr-&6l0++$#11e_ny4ivoeyexm6ebeyekv8ntga5l$bymur41ss"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['*']
