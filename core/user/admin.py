e3f118bcee5de9fe4d670e58e861b286ad54c433
KirGorod <kgorodisky@gmail.com>
2023-08-17 16:11:22 +0000

    update user admin with avatar field

--- core/user/admin.py
+++ core/user/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
 from user.models import User
-from django.contrib.auth.admin import UserAdmin
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "avatar", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
 
 admin.site.register(User, UserAdmin)
