4955c42bc69a47bca5f81e4f07acfcfb07088e3c
KirGorod <kgorodisky@gmail.com>
2023-08-24 14:16:11 +0300

    random card enpoint for testing

--- core/cards/urls.py
+++ core/cards/urls.py
@@ -1,4 +1,5 @@
 from . import views
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 app_name = 'cards'
@@ -7,3 +8,6 @@ app_name = 'cards'
 router = DefaultRouter()
 router.register(r'cards', views.CardViewSet, basename='cards')
 urlpatterns = router.urls
+urlpatterns += [
+    path('random_card/', views.RandomCardView.as_view(), name='random_card'),
+]
--- core/cards/views.py
+++ core/cards/views.py
@@ -1,4 +1,9 @@
+import random
+
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from cards.models import Card
 from .serializers import CardSerializer
@@ -8,3 +13,17 @@ class CardViewSet(viewsets.ModelViewSet):
     model = Card
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
+
+class RandomCardView(APIView):
+    def get(self, request, *args, **kwargs):
+        cards = Card.objects.all()
+        if not cards:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        random_card = random.choice(cards)
+        serializer = CardSerializer(instance=random_card)
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK
+        )
