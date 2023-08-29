8485d8bf3f32186503736a6775bb2015ee6990c6
KirGorod <kgorodisky@gmail.com>
2023-08-29 18:36:57 +0300

    cards tests for post, patch, delete

--- core/cards/tests/test_api.py
+++ core/cards/tests/test_api.py
@@ -1,3 +1,4 @@
+import json
 from collections import OrderedDict
 
 from django.urls import reverse
@@ -15,8 +16,8 @@ class TestCards(APITestCase):
         self.cards = CardFactory.create_batch(5, deck=self.deck)
         self.user = self.deck.user
 
-        deck2 = DeckFactory.create()
-        CardFactory.create_batch(3, deck=deck2)
+        self.deck2 = DeckFactory.create()
+        self.cards2 = CardFactory.create_batch(3, deck=self.deck2)
 
     def test_get_single_card_not_authorized(self):
         """
@@ -31,7 +32,7 @@ class TestCards(APITestCase):
 
     def test_get_single_card_success(self):
         """
-        Test successfull GET single card object
+        Test successful GET single card object
         """
         card = Card.objects.filter(deck__user=self.user).first()
         self.client.force_authenticate(self.user)
@@ -53,9 +54,9 @@ class TestCards(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_get_carsd_list_success(self):
+    def test_get_cards_list_success(self):
         """
-        Test successfull GET list of cards
+        Test successful GET list of cards
         """
         self.client.force_authenticate(self.user)
         response = self.client.get(
@@ -71,6 +72,166 @@ class TestCards(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, cards)
+
+    def test_create_card_not_authorized(self):
+        """
+        Test create card when user is not authenticated
+        """
+        data = {
+            'deck': self.deck.id,
+            'word': 'apple',
+            'translation': 'яблуко',
+            'description': 'some description',
+            # 'image': ''
+        }
+        response = self.client.post(
+            reverse('cards:cards-list'),
+            data=data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_card_in_not_owned_deck(self):
+        """
+        Test create card inside not owned deck by this user
+        """
+        self.client.force_authenticate(self.user)
+        data = {
+            'deck': self.deck2.id,
+            'word': 'apple',
+            'translation': 'яблуко',
+            'description': 'some description',
+            # 'image': ''
+        }
+        response = self.client.post(
+            reverse('cards:cards-list'),
+            data=data,
+            format='json'
+        )
+
+        error_message = json.loads(response.content.decode()).get('deck')[0]
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            error_message,
+            'This deck cannot be edited by this user.'
+        )
+
+    def test_create_card_success(self):
+        """
+        Test successful creation of a card
+        """
+        self.client.force_authenticate(self.user)
+        data = {
+            'deck': self.deck.id,
+            'word': 'apple',
+            'translation': 'яблуко',
+            'description': 'some description',
+            # 'image': ''
+        }
+        response = self.client.post(
+            reverse('cards:cards-list'),
+            data=data,
+            format='json'
+        )
+        card = Card.objects.last()
+        expected_response = self._get_expected_response(card)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertDictEqual(response.data, expected_response)
+
+    def test_patch_card_not_authorized(self):
+        """
+        Test partial update on card when user is not authenticated
+        """
+        card = self.cards[0]
+        data = {
+            'word': 'new word',
+            'translation': 'new_translation'
+        }
+        response = self.client.patch(
+            reverse('cards:cards-detail', kwargs={'pk': card.id}),
+            data=data,
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_patch_not_owned_card(self):
+        """
+        Test partial update on card that is not owned by user
+        """
+        self.client.force_authenticate(self.user)
+        card = self.cards2[0]
+        data = {
+            'word': 'new word',
+            'translation': 'new_translation'
+        }
+        response = self.client.patch(
+            reverse('cards:cards-detail', kwargs={'pk': card.id}),
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_patch_card_success(self):
+        """
+        Test partial update successful
+        """
+        self.client.force_authenticate(self.user)
+        card = self.cards[0]
+        data = {
+            'word': 'new word',
+            'translation': 'new_translation'
+        }
+        response = self.client.patch(
+            reverse('cards:cards-detail', kwargs={'pk': card.id}),
+            data=data,
+            format='json'
+        )
+        expected_response = self._get_expected_response(
+            Card.objects.get(id=card.id)
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertDictEqual(response.data, expected_response)
+
+    def test_delete_card_not_authenticated(self):
+        """
+        Test DELETE card without authentication
+        """
+        card = self.cards[0]
+        response = self.client.delete(
+            reverse('cards:cards-detail', kwargs={'pk': card.id})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertTrue(Card.objects.filter(id=card.id).exists())
+
+    def test_delete_card_not_owned_by_user(self):
+        """
+        Test DELETE card that is not owned by user
+        """
+        self.client.force_authenticate(self.user)
+        card = self.cards2[0]
+        response = self.client.delete(
+            reverse('cards:cards-detail', kwargs={'pk': card.id})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue(Card.objects.filter(id=card.id).exists())
+
+    def test_delete_card_success(self):
+        """
+        Test successful card deletion
+        """
+        self.client.force_authenticate(self.user)
+        card = self.cards[0]
+        response = self.client.delete(
+            reverse('cards:cards-detail', kwargs={'pk': card.id})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Card.objects.filter(id=card.id).exists())
 
     def _get_expected_response(self, card: Card) -> dict:
         """
