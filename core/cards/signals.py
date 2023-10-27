93cfd126e1199820f558ab700ae1b47009bdd82a
KirGorod <kgorodisky@gmail.com>
2023-10-27 18:08:06 +0300

    generate image hash inside celery task

--- core/cards/signals.py
+++ core/cards/signals.py
@@ -1,21 +1,22 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .utils import set_additional_images, generate_hashed_images
+from .utils import set_additional_images
+from .tasks import generate_hashed_images
 from .models import Card, Deck, CardAdditionalImage
 
 
 @receiver(post_save, sender=Card)
 def set_card_images(sender, instance, created, **kwargs):
     if created:
-        generate_hashed_images(instance)
+        generate_hashed_images.delay('cards.Card', instance.id)
 
     old_instance = sender.objects.get(pk=instance.pk)
     old_image = old_instance.image
     new_image = instance.__dict__.get("image", None)
 
     if old_image != new_image:
-        generate_hashed_images(instance)
+        generate_hashed_images.delay('cards.Card', instance.id)
 
     set_additional_images(instance)
 
@@ -23,24 +24,24 @@ def set_card_images(sender, instance, created, **kwargs):
 @receiver(post_save, sender=Deck)
 def set_deck_images(sender, instance, created, **kwargs):
     if created:
-        generate_hashed_images(instance)
+        generate_hashed_images.delay('cards.Deck', instance.id)
 
     old_instance = sender.objects.get(pk=instance.pk)
     old_image = old_instance.image
     new_image = instance.__dict__.get("image", None)
 
     if old_image != new_image:
-        generate_hashed_images(instance)
+        generate_hashed_images.delay('cards.Deck', instance.id)
 
 
 @receiver(post_save, sender=CardAdditionalImage)
 def set_card_additional_images(sender, instance, created, **kwargs):
     if created:
-        generate_hashed_images(instance)
+        generate_hashed_images.delay('cards.CardAdditionalImage', instance.id)
 
     old_instance = sender.objects.get(pk=instance.pk)
     old_image = old_instance.image
     new_image = instance.__dict__.get("image", None)
 
     if old_image != new_image:
-        generate_hashed_images(instance)
+        generate_hashed_images.delay('cards.CardAdditionalImage', instance.id)
--- core/cards/tasks.py
+++ core/cards/tasks.py
@@ -1,10 +1,29 @@
+import blurhash
+
+from django.apps import apps
 from core.celery import app as celery_app
-from celery import shared_task
 
 
 @celery_app.task(name='cards.tasks.add_numbers')
-# @shared_task
 def add_numbers(a, b):
     res = a + b
     print(res)
     return res
+
+
+@celery_app.task(name='cards.tasks.generate_hashed_images')
+def generate_hashed_images(modelName, instance_id):
+    module, model = modelName.split('.')
+    instance_model = apps.get_model(module, model)
+    instance = instance_model.objects.get(id=instance_id)
+
+    if not instance.image:
+        return
+
+    try:
+        with open(instance.image.path, 'rb') as image_file:
+            hash = blurhash.encode(image_file, x_components=4, y_components=3)
+            instance.image_hash = hash
+            instance.save()
+    except FileNotFoundError:
+        pass
