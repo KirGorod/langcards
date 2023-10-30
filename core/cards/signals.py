3bd875bc995fc7cb58dd0484d0b8980a49db4e3c
KirGorod <kgorodisky@gmail.com>
2023-10-30 17:20:46 +0200

    refactor

--- core/cards/signals.py
+++ core/cards/signals.py
@@ -7,34 +7,26 @@ from .tasks import generate_hashed_images
 from .models import Card, Deck, CardAdditionalImage
 
 
-@receiver(post_save, sender=Card)
-def set_card_images(sender, instance, created, **kwargs):
+def generate_hash(created, model_name, instance):
     if created:
         transaction.on_commit(
-            lambda: generate_hashed_images.delay('cards.Card', instance.pk)
+            lambda: generate_hashed_images.delay(model_name, instance.pk)
         )
     else:
-        generate_hashed_images.delay('cards.Card', instance.id)
+        generate_hashed_images.delay(model_name, instance.id)
+
+
+@receiver(post_save, sender=Card)
+def set_card_images(sender, instance, created, **kwargs):
+    generate_hash(created, 'cards.Card', instance)
     set_additional_images(instance)
 
 
 @receiver(post_save, sender=Deck)
 def set_deck_images(sender, instance, created, **kwargs):
-    if created:
-        transaction.on_commit(
-            lambda: generate_hashed_images.delay('cards.Deck', instance.pk)
-        )
-    else:
-        generate_hashed_images.delay('cards.Deck', instance.id)
+    generate_hash(created, 'cards.Deck', instance)
 
 
 @receiver(post_save, sender=CardAdditionalImage)
 def set_card_additional_images(sender, instance, created, **kwargs):
-    if created:
-        transaction.on_commit(
-            lambda: generate_hashed_images.delay(
-                'cards.CardAdditionalImage', instance.pk
-            )
-        )
-    else:
-        generate_hashed_images.delay('cards.CardAdditionalImage', instance.id)
+    generate_hash(created, 'cards.CardAdditionalImage', instance)
