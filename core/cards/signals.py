a653dbdb98230705f591115d1f32eff7bfa79375
KirGorod <kgorodisky@gmail.com>
2023-10-30 17:02:47 +0200

    fix generate hash for created

--- core/cards/signals.py
+++ core/cards/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save
+from django.db import transaction
 from django.dispatch import receiver
 
 from .utils import set_additional_images
@@ -8,15 +9,29 @@ from .models import Card, Deck, CardAdditionalImage
 
 @receiver(post_save, sender=Card, )
 def set_card_images(sender, instance, created, **kwargs):
+    if created:
+        transaction.on_commit(
+            lambda: generate_hashed_images.delay('cards.Card', instance.pk)
+        )
     generate_hashed_images.delay('cards.Card', instance.id)
     set_additional_images(instance)
 
 
 @receiver(post_save, sender=Deck)
 def set_deck_images(sender, instance, created, **kwargs):
+    if created:
+        transaction.on_commit(
+            lambda: generate_hashed_images.delay('cards.Deck', instance.pk)
+        )
     generate_hashed_images.delay('cards.Deck', instance.id)
 
 
 @receiver(post_save, sender=CardAdditionalImage)
 def set_card_additional_images(sender, instance, created, **kwargs):
+    if created:
+        transaction.on_commit(
+            lambda: generate_hashed_images.delay(
+                'cards.CardAdditionalImage', instance.pk
+            )
+        )
     generate_hashed_images.delay('cards.CardAdditionalImage', instance.id)
